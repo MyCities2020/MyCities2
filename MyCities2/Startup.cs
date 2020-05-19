988a349feb919d6ef7851ba9ff80d039949e13d6
Paolo Socrates <paul_socrate@hotmail.fr>
2020-05-19 20:44:59 +0200

    Login/Register

--- MyCities2/Startup.cs
+++ MyCities2/Startup.cs
@@ -27,6 +27,7 @@ namespace MyCities2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddControllersWithViews();
             services.AddDbContext<Models.MyCitiesDBContext>(options =>
             {
